82e8142feea936945e44461e00fc03b419181d8a
Lucaszioli <lucas.zioli.04@gmail.com>
2025-05-02 17:31:51 -0300

    feat: adiciona os inits das atividades

--- app/models/atividade.py
+++ app/models/atividade.py
@@ -9,5 +9,43 @@ class TipoAtividade(Enum):
     REVISAO = "Aula de revisão"
 
 class Atividade:
-    def __init__(self):
+    def __init__(self, nome, data, id_disciplina, observacoes = None):
+        self.nome = nome
+        self.data = data
+        self.id_disciplina = id_disciplina
+        self.observacoes = observacoes
         pass
+
+
+class Trabalho(Atividade):
+    def __init__(self, nome, data, id_disciplina, nota_maxima, nota = None, observacoes = None):
+        super().__init__(nome, data, id_disciplina, observacoes)
+        self.tipo = TipoAtividade.TRABALHO
+        self.nota_maxima = nota_maxima
+        self.nota = nota
+
+class Prova(Atividade):
+    def __init__(self, nome, data, id_disciplina, nota_maxima, nota = None, observacoes = None):
+        super().__init__(nome, data, id_disciplina, observacoes)
+        self.tipo = TipoAtividade.PROVA
+        self.nota_maxima = nota_maxima
+        self.nota = nota
+
+class Aula_de_Campo(Atividade):
+    def __init__(self, nome, data, id_disciplina, observacoes = None):
+        super().__init__(nome, data, id_disciplina, observacoes)
+        self.tipo = TipoAtividade.CAMPO
+
+class Apresentacao(Atividade):
+    def __init__(self, nome, data, id_disciplina, nota_maxima, nota = None, observacoes = None):
+        super().__init__(nome, data, id_disciplina, observacoes)
+        self.tipo = TipoAtividade.APRESENTACAO
+        self.nota_maxima = nota_maxima
+        self.nota = nota
+
+
+class Revisao(Atividade):
+    def __init__(self, nome, data, id_disciplina, observacoes = None, nota = None):
+        super().__init__(nome, data, id_disciplina, observacoes)
+        self.tipo = TipoAtividade.REVISAO
+        self.nota = nota
