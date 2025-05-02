a8501cc670c34d5779b3475fc66088a95b6c627a
Lucaszioli <lucas.zioli.04@gmail.com>
2025-05-02 17:55:11 -0300

    Adiciona funcao de teste para o bd

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from app.utils.database import Database
+from app.models.atividade import Atividade, Trabalho
 
 def main():
     # Inicializar o banco de dados e criar tabelas usando o script SQL
     db = Database(db_path="db.db")
     db.criar("app/scripts/init.sql")
+
+    atividade = Trabalho("Trabalho de Matemática", "2023-10-15", 1, 5, 4, "Entregar até o dia 20")
+    atividade.adicionar_bd(db.conectar())
 
     print("Tabelas criadas com sucesso!")
 
