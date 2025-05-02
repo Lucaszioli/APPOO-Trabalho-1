79c4d9e693f7b7b22128af18947b7f08ffd254a9
Lucaszioli <lucas.zioli.04@gmail.com>
2025-05-02 17:31:38 -0300

    feat: Adiciona os inits das atividades

--- app/models/atividade.py
+++ app/models/atividade.py
@@ -1,3 +1,13 @@
+from abc import ABC, abstractmethod
+from enum import Enum
+
+class TipoAtividade(Enum):
+    TRABALHO = "Trabalho"
+    PROVA = "Prova"
+    CAMPO = "Aula de campo"
+    APRESENTACAO = "Apresentação de Trabalho"
+    REVISAO = "Aula de revisão"
+
 class Atividade:
     def __init__(self):
         pass
