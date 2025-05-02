e0a021effeb0bac2956898ee743c6df1d76d4aa5
Henrique Dias <henriquejndg@gmail.com>
2025-05-02 20:13:36 -0300

    feat: implementa a classe Disciplina com métodos para adicionar, editar e deletar no banco de dados

---
+++ app/models/disciplinas.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+from enum import Enum
+
+class Disciplina(ABC):
+    def __init__(self, nome, carga_horaria, semestre_id, observacao = None, id = None):
+        self.nome = nome
+        self.carga_horaria = carga_horaria
+        self.semestre_id = semestre_id
+        self.atividades = []
+        self.observacao = observacao
+        self.id = id
+        pass
+
+    def adicionar_bd(self, conexao):
+        cursor = conexao.cursor()
+        cursor.execute("INSERT INTO disciplina (nome, carga_horaria, semestre_id, observacao) VALUES (?, ?, ?, ?)", (self.nome, self.carga_horaria, self.semestre_id, self.observacao))
+        conexao.commit()
+        pass
+    
+    def editar_bd(self, conexao):
+        cursor = conexao.cursor()
+        cursor.execute("UPDATE disciplina SET nome = ?, carga_horaria = ?, semestre_id = ?, observacao = ? WHERE id = ?", (self.nome, self.carga_horaria, self.semestre_id, self.observacao, self.id))
+        conexao.commit()
+        pass
+    
+    def deletar_bd(self, conexao):
+        cursor = conexao.cursor()
+        cursor.execute("DELETE FROM disciplina WHERE id = ?", (self.id,))
+        conexao.commit()
+        pass
+    
+    
