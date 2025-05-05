e45718a4a8840726a8126923fbe38c3044597356
Henrique Dias <henriquejndg@gmail.com>
2025-05-05 16:08:25 -0300

    feat: enhance sidebar with scaling options and appearance mode functionality

--- app/views/pagina_inicial.py
+++ app/views/pagina_inicial.py
@@ -8,27 +8,58 @@ customtkinter.set_default_color_theme("app/themes/rose.json")  # Themes: "blue",
 class PaginaInicial(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+        
+        # Configurações da janela
         self.title("Sistema de Gerenciamento Acadêmico")
         self.geometry("1000x600")
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
+        
+        # Barra lateral
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Menu", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        
+        # Sidebar buttons
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, text="Botão 1")
+        self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
+        
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Botão 2")
+        self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
+        
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, text="Botão 3")
+        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+        
+        # Sidebar configs
+        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Aparência:", anchor="w")
+        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Claro", "Escuro", "Sistema"],
+                                                                       command=self.change_appearance_mode_event)
+        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+        
+        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="Escala UI:", anchor="w")
+        self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
+                                                               command=self.change_scaling_event)
+        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
+        
+    # Funções
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        if new_appearance_mode == "Claro":
+            new_appearance_mode = "Light"
+        elif new_appearance_mode == "Escuro":
+            new_appearance_mode = "Dark"
+        elif new_appearance_mode == "Sistema":
+            new_appearance_mode = "System"
+        customtkinter.set_appearance_mode(new_appearance_mode)
 
-        # Main
-        main = customtkinter.CTkFrame(self)
-        main.grid(row=0, column=1, sticky="nsew")
-        main.grid_columnconfigure(0, weight=1)
+    def change_scaling_event(self, new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
         
-        # Header
-        header = customtkinter.CTkFrame(main)
-        header.grid(row=0, column=0, sticky="nsew")
-        header.grid_columnconfigure(0, weight=1)
-        header.grid_rowconfigure(0, weight=1)
         
-        # Title
-        title = customtkinter.CTkLabel(header, text="Sistema de Gerenciamento Acadêmico")
-        title.grid(row=0, column=0, padx=20, pady=20)
-        title.grid_columnconfigure(0, weight=1)
-        title.grid_rowconfigure(0, weight=1)
-        title.configure(font=("CTkFont", 24))
+        
     
         
