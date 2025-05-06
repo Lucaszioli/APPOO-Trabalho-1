9174df2a200a2976aca25295226c94f0f90a08cd
Henrique Dias <henriquejndg@gmail.com>
2025-05-06 17:39:16 -0300

    feat: remove semester creation and discipline addition from main function

--- main.py
+++ main.py
@@ -8,16 +8,6 @@ def main():
     db = Database(db_path="db.db")
     db.criar("app/scripts/init.sql")
     conexao = db.conectar()
-    
-    # Criar um semestre
-    semestre = SemestreService.criar("Teste", "Inicio", "Fim", conexao )
-    
-    # Adicionar disciplinas ao semestre
-    DisciplinaServices.criar(nome="Teste1", semestre=semestre,codigo="MAT60", carga_horaria=60, conexao=conexao)
-    DisciplinaServices.criar(nome="Teste1", semestre=semestre,codigo="MAT61", carga_horaria=60, conexao=conexao)
-    semestre.listar_disciplinas()
-    
-    print("Tabelas criadas com sucesso!")
 
     app = PaginaInicial(conexao)
     app.mainloop()
