2d5af95911fe836dec3f4caeed747eceece9ce1f
Henrique Dias <henriquejndg@gmail.com>
2025-05-07 11:19:20 -0300

    feat: initialize PaginaSemestre with basic window configuration

---
+++ app/views/pagina_semestre.py
@@ -0,0 +1,17 @@
+import tkinter
+import tkinter.messagebox
+import customtkinter
+
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+
+class PaginaSemestre(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # configure window
+        self.title("Semestre.py")
+        self.geometry(f"{1100}x{580}")
+
+       
