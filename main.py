e0bd8791ee19716e20ce85bfea46dbd50c17c9f2
Lucaszioli <lucas.zioli.04@gmail.com>
2025-05-13 17:02:13 -0300

    refactor: simplify main function by removing redundant database connection steps

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ from app.services.disciplinas_services import DisciplinaServices
 def main():
     # Inicializar o banco de dados e criar tabelas usando o script SQL
     db = Database(db_path="db.db")
-    db.criar("app/scripts/init.sql")
-    conexao = db.conectar()
-
-    app = PaginaInicial(conexao)
+    
+    app = PaginaInicial(db.conexao)
     app.mainloop()
     
 if __name__ == "__main__":
