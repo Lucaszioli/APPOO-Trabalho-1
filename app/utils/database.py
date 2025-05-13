8df38604aad530a66e6289da3bef2d3e7f5e0c01
Lucaszioli <lucas.zioli.04@gmail.com>
2025-05-13 17:01:00 -0300

    refactor: improve database connection and initialization methods

--- app/utils/database.py
+++ app/utils/database.py
@@ -2,17 +2,56 @@ import sqlite3
 class Database:
     def __init__(self, db_path="db.db"):
         self.db_path = db_path
+        self.conexao = None
+        self.__criar(script_path="app/scripts/init.sql")
+        self.conexao = self.__conectar()
 
-    def conectar(self):
-        conexao = sqlite3.connect(self.db_path)
-        conexao.execute("PRAGMA foreign_keys = ON")
-        return conexao
+    def __conectar(self):
+        if not self.conexao:
+            self.conexao = sqlite3.connect(self.db_path)
+            self.conexao.execute("PRAGMA foreign_keys = ON")
+        return self.conexao
     
-    def criar(self, script_path):
+    def __criar(self, script_path):
         with open(script_path, 'r', encoding='utf-8') as script_file:
             script = script_file.read()
         
-        with self.conectar() as conexao:
+        with self.__conectar() as conexao:
             cursor = conexao.cursor()
             cursor.executescript(script)
             conexao.commit()
+
+    @staticmethod
+    def _adicionar(query, params, conexao):
+        cursor = conexao.cursor()
+        cursor.execute(query, params)
+        conexao.commit()
+        return cursor.lastrowid
+    
+    @staticmethod
+    def _editar(query, params, conexao):
+        cursor = conexao.cursor()
+        cursor.execute(query, params)
+        conexao.commit()
+        return cursor.rowcount
+    
+    @staticmethod
+    def _buscar_um(query, params, conexao):
+        cursor = conexao.cursor()
+        cursor.execute(query, params)
+        row = cursor.fetchone()
+        return row
+    
+    @staticmethod
+    def _buscar_varios(query, params, conexao):
+        cursor = conexao.cursor()
+        cursor.execute(query, params)
+        rows = cursor.fetchall()
+        return rows
+    
+    @staticmethod
+    def _deletar(query, params, conexao):
+        cursor = conexao.cursor()
+        cursor.execute(query, params)
+        conexao.commit()
+        return cursor.rowcount
