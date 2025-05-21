ed8f1aa94ca1352db23de4be238b548d3b520dc1
Lucas Zioli <lucas.zioli.04@gmail.com>
2025-05-20 21:15:21 -0300

    feat: Adiciona semestre universal

---
+++ app/services/service_universal.py
@@ -0,0 +1,5 @@
+class ServiceUniversal :
+    def __init__(self, semestre_service, disciplina_service):
+        self.semestre_service = semestre_service
+        self.disciplina_service = disciplina_service
+        # self.atividade_service = atividade_service
