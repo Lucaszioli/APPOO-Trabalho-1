7b70d866f5cd3288c4d1300752d90e07654f61ad
Lucas Zioli <lucas.zioli.04@gmail.com>
2025-05-21 21:39:04 -0300

    fix: update type hints in AtividadeService methods for better clarity and consistency

--- app/services/atividade_services.py
+++ app/services/atividade_services.py
@@ -2,14 +2,14 @@ from abc import ABC, abstractmethod
 from app.utils.database import Database
 from app.models.disciplinas import Disciplina
 from app.models.atividade import Atividade, Trabalho, Prova, Aula_de_Campo, Revisao,TipoAtividadeEnum
-from typing import Type
+from typing import Optional
 from app.errors.notFound import AtividadeNotFoundError
 class AtividadeService(ABC, Database):
 
     def __init__(self, db_path="db.db"):
         super().__init__(db_path)
 
-    def _adicionar_bd(self, atividade:Type[Atividade]) -> Type[Atividade]:
+    def _adicionar_bd(self, atividade:"Atividade") -> Atividade:
         self.query = "INSERT INTO atividade (nome, data, disciplina_id, tipo, nota_total, nota, observacao, lugar, data_apresentacao) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
         self.params = (atividade.nome, atividade.data,atividade.disciplina_id, atividade.tipo, atividade.nota_total, atividade.nota, atividade.observacao, atividade.lugar, atividade.data_apresentacao)
         self._adicionar(self.query, self.params)
@@ -33,7 +33,7 @@ class AtividadeService(ABC, Database):
         return [Atividade(id=row[0], nome=row[1], data=row[2], nota=row[3], nota_total=row[4], disciplina_id=row[5], tipo=row[6], observacao=row[7], lugar=row[8], data_apresentacao=row[9]) for row in atividades] 
 
     
-    def buscar_por_id(self, id:str) -> Type[Atividade]:
+    def buscar_por_id(self, id:str) -> Optional[Atividade]:
         self.query = "SELECT * FROM atividade WHERE id = ?"
         self.params = (id,)
         atividade = self._buscar_um(self.query, self.params)
@@ -41,7 +41,7 @@ class AtividadeService(ABC, Database):
             return Atividade(id=atividade[0], nome=atividade[1], data=atividade[2], nota=atividade[3], nota_total=atividade[4], disciplina_id=atividade[5], tipo=atividade[6], observacao=atividade[7], lugar=atividade[8], data_apresentacao=atividade[9])
         return None
     
-    def editar_bd(self, atividade:Type[Atividade]) -> Type[Atividade]:
+    def editar_bd(self, atividade:"Atividade") -> "Atividade":
         self.atividadeExistente = self.buscar_por_id(atividade.id)
         if not self.atividadeExistente:
             raise AtividadeNotFoundError()
@@ -50,7 +50,7 @@ class AtividadeService(ABC, Database):
         self._editar(self.query, self.params)
         return atividade
     
-    def deletar(self, atividade:Type[Atividade]) -> Type[Atividade]:
+    def deletar(self, atividade:"Atividade") -> Atividade:
         self.atividadeExistente = self.buscar_por_id(atividade.id)
         if not self.atividadeExistente:
             raise AtividadeNotFoundError()
@@ -60,7 +60,7 @@ class AtividadeService(ABC, Database):
         del atividade
         return self.rows
     
-    def criar_atividade(self, tipo:Type[Atividade], nome:str, data:str, disciplina:"Disciplina", nota_total:int, nota:int = None, observacao:str = None, lugar:str = None, data_apresentacao:str = None) -> Type[Atividade]:
+    def criar_atividade(self, tipo:"TipoAtividadeEnum", nome:str, data:str, disciplina:"Disciplina", nota_total:int, nota:int = None, observacao:str = None, lugar:str = None, data_apresentacao:str = None) -> Atividade:
         if tipo == TipoAtividadeEnum().TRABALHO:
             atividade = Trabalho(nome, data, disciplina.id, nota_total, nota=nota, observacao=observacao, data_apresentacao=data_apresentacao)
         elif tipo == TipoAtividadeEnum().PROVA:
