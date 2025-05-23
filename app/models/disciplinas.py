33482ed1e5d2ae37855cc061934f5cd15fdd2460
Lucas Zioli <lucas.zioli.04@gmail.com>
2025-05-22 21:26:36 -0300

    refactor: add type hints to Disciplina class constructor and adicionar_atividade method for improved clarity

--- app/models/disciplinas.py
+++ app/models/disciplinas.py
@@ -1,5 +1,15 @@
+from typing import Optional
+from app.models.atividade import Atividade
 class Disciplina:
-    def __init__(self, nome, carga_horaria, semestre_id, codigo, observacao = None, id = None):
+    def __init__(
+        self, 
+        nome: str, 
+        carga_horaria: int, 
+        semestre_id:int, 
+        codigo:str, 
+        observacao: Optional[str] = None, 
+        id = None
+        ):
         self.nome = nome
         self.carga_horaria = carga_horaria
         self.semestre_id = semestre_id
@@ -8,7 +18,10 @@ class Disciplina:
         self.observacao = observacao
         self.id = id
 
-    def adicionar_atividade(self, atividade):
+    def adicionar_atividade(
+    self, 
+    atividade: "Atividade"
+    ):
         self.atividades.append(atividade)
 
     def listar_atividades(self):
