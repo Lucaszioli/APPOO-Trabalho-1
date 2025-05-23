68739536ca67512f4b4017a98e3a8370c4b18608
Lucas Zioli <lucas.zioli.04@gmail.com>
2025-05-22 22:05:38 -0300

    refactor: add type hints and property methods to Semestre class for improved clarity and consistency

--- app/models/semestre.py
+++ app/models/semestre.py
@@ -1,23 +1,54 @@
+from typing import Optional
+from app.models.disciplinas import Disciplina
 class Semestre:
-    def __init__(self, nome, data_inicio, data_fim, id=None):
-        self.id = id
-        self.nome = nome
-        self.data_inicio = data_inicio
-        self.data_fim = data_fim
-        self.disciplinas = []
+    def __init__(
+    self, 
+    nome: str, 
+    data_inicio: str, 
+    data_fim: str, 
+    id: Optional[int]=None
+    ):
+        self._id = id
+        self._nome = nome
+        self._data_inicio = data_inicio
+        self._data_fim = data_fim
+        self._disciplinas = []
 
-    def set_nome(self, nome):
-        self.nome = nome
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome: str):
+        self._nome = nome
+
+    @property
+    def id(self):
+        return self._id
     
-    def set_data_inicio(self, data_inicio):
-        self.data_inicio = data_inicio
+    @id.setter
+    def id(self, id: int):
+        self._id = id
+
+    @property
+    def data_inicio(self):
+        return self._data_inicio
     
-    def set_data_fim(self, data_fim):
-        self.data_fim = data_fim
+    @data_inicio.setter
+    def data_inicio(self, data_inicio: str):
+        self._data_inicio = data_inicio
+
+    @property
+    def data_fim(self):
+        return self._data_fim
     
-    def adicionar_disciplina(self, disciplina):
-        self.disciplinas.append(disciplina)
+    @data_fim.setter
+    def data_fim(self, data_fim: str):
+        self._data_fim = data_fim
+    
+    def adicionar_disciplina(self, disciplina: "Disciplina"):
+        self._disciplinas.append(disciplina)
    
     def listar_disciplinas(self):
-        for disciplina in self.disciplinas:
+        for disciplina in self._disciplinas:
             print(f"Disciplina: {disciplina.nome}, Carga Horária: {disciplina.carga_horaria}, Código: {disciplina.codigo}, Observação: {disciplina.observacao}, Id: {disciplina.id}")
