542eaf769558746f02e914500a7ccab8ae42c8a8
Lucaszioli <lucas.zioli.04@gmail.com>
2025-05-25 18:58:31 -0300

    fix: correct attribute access for database connection in PaginaInicial instantiation

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     db = Database(db_path="db.db")
     semestre_service = SemestreService(db_path="db.db")
     disciplina_service = DisciplinaService(db_path="db.db")
-    app = PaginaInicial(db.conexao,semestre_service=semestre_service, disciplina_service=disciplina_service)
+    app = PaginaInicial(db._conexao,semestre_service=semestre_service, disciplina_service=disciplina_service)
     app.mainloop()
     
 if __name__ == "__main__":
