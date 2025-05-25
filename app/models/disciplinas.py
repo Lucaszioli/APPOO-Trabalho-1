5d82f753bc965bf2558fabcde270bc76268bf981
Lucaszioli <lucas.zioli.04@gmail.com>
2025-05-25 16:36:53 -0300

    refactor: improve Disciplina class with encapsulation and property methods for better data management

--- app/models/disciplinas.py
+++ app/models/disciplinas.py
@@ -8,21 +8,69 @@ class Disciplina:
         semestre_id:int, 
         codigo:str, 
         observacao: Optional[str] = None, 
-        id = None
+        id:Optional[int] = None
         ):
-        self.nome = nome
-        self.carga_horaria = carga_horaria
-        self.semestre_id = semestre_id
-        self.atividades = []
-        self.codigo = codigo
-        self.observacao = observacao
-        self.id = id
+        self._nome = nome
+        self._carga_horaria = carga_horaria
+        self._semestre_id = semestre_id
+        self._atividades = []
+        self._codigo = codigo
+        self._observacao = observacao
+        self._id = id
+
+    @property
+    def nome(self) -> str:
+        return self._nome
+    @nome.setter
+    def nome(self, nome: str) -> None:
+        self._nome = nome
+
+    @property
+    def carga_horaria(self) -> int:
+        return self._carga_horaria
+    
+    @carga_horaria.setter
+    def carga_horaria(self, carga_horaria: int) -> None:
+        self._carga_horaria = carga_horaria
+    
+    @property
+    def semestre_id(self) -> int:
+        return self._semestre_id
+    
+    @semestre_id.setter
+    def semestre_id(self, semestre_id: int) -> None:
+        self._semestre_id = semestre_id
+
+    @property
+    def id(self) -> Optional[int]:
+        return self._id
+    
+    @id.setter
+    def id(self, id: int) -> None:
+        self._id = id
+
+    @property
+    def codigo(self) -> str:
+        return self._codigo
+    
+    @codigo.setter
+    def codigo(self, codigo: str) -> None:
+        self._codigo = codigo
+
+    @property
+    def observacao(self) -> Optional[str]:
+        return self._observacao
+    
+    @observacao.setter
+    def observacao(self, observacao: str) -> None:
+        self._observacao = observacao
+    
 
     def adicionar_atividade(
     self, 
     atividade: "Atividade"
     ) -> None:
-        self.atividades.append(atividade)
+        self._atividades.append(atividade)
 
     def listar_atividades(self) -> None:
         for atividade in self.atividades:
