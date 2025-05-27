59cddb165d8b2d28b1e11dfce1c30d3f3ce2275a
Lucas Zioli <lucas.zioli.04@gmail.com>
2025-05-26 22:02:12 -0300

    fix: initialize AtividadeService in ServiceUniversal constructor

--- app/services/service_universal.py
+++ app/services/service_universal.py
@@ -1,7 +1,9 @@
 from app.services.semestre_services import SemestreService
 from app.services.disciplinas_services import DisciplinaService
+from app.services.atividade_services import AtividadeService
 class ServiceUniversal :
-    def __init__(self, semestre_service:"SemestreService", disciplina_service:"DisciplinaService"):
-        self.semestre_service = semestre_service
-        self.disciplina_service = disciplina_service
+    def __init__(self, db_path="db.db"):
+        self.semestre_service = SemestreService(db_path=db_path)
+        self.disciplina_service = DisciplinaService(db_path=db_path)
+        self.atividade_service = AtividadeService(db_path=db_path)
         # self.atividade_service = atividade_service
